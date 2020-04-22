4b82b182df90a39b6e2e60456b6a5cddf671c86c
Eric <MKme@users.noreply.github.com>
2020-04-21 20:13:12 -0400

    Update erics_testDatalogger.ino

--- SPI SD Card/erics_testDatalogger/erics_testDatalogger.ino
+++ SPI SD Card/erics_testDatalogger/erics_testDatalogger.ino
@@ -38,7 +38,7 @@ const int chipSelect = D4;
 void setup()
 {
   // Open serial communications and wait for port to open:
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
